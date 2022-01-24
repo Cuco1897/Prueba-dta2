bb43e3e5524e11138d0d50391ab9d5fe7fb40b17
Cuco1897 <32716183+Cuco1897@users.noreply.github.com>
2022-01-24 09:44:33 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8"/>
+<!-- Set the viewport width to device width for mobile -->
+<meta name="viewport" content="width=device-width"/>
+<title>DTA-INICIO</title>
+<!-- CSS Files-->
+<link rel="stylesheet" href="stylesheets/style.css">
+
+<link rel="stylesheet" href="stylesheets/skins/blue.css">
+<!-- skin color -->
+<link rel="stylesheet" href="stylesheets/responsive.css">
+<!-- IE Fix for HTML5 Tags -->
+<!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+<!-- HIDDEN PANEL 
+================================================== -->
+<div id="panel">
+	<div class="row">
+		<div class="twelve columns">
+			<img src="http://www.wowthemes.net/demo/studiofrancesca/images/info.png" class="pics" alt="info">
+			<div class="infotext">
+				 Gracias por Visitar De todo en Alambre!
+			</div>
+		</div>
+	</div>
+</div>
+<p class="slide">
+	<a href="#" class="btn-slide"></a>
+</p>
+<!-- HEADER
+================================================== -->
+<div class="row">
+	<div class="headerlogo four columns">
+		<div class="logo">
+			<a href="index.html">
+			<h4>De todo en Alambre </h4>
+			</a>
+		</div>
+	</div>
+	<div class="headermenu eight columns noleftmarg">
+		<nav id="nav-wrap">
+		<ul id="main-menu" class="nav-bar sf-menu">
+			<li class="current">
+			<a href="index.html">Inicio</a>
+			</li>
+			<li>
+			<a href="#">Productos</a>
+			<ul>
+				<li><a href="portofolio2.html">Exhibidores</a></li>
+				<li><a href="portofolio3.html">OEM</a></li>
+				<li><a href="portofolio4.html">Electrodomésticos</a></li>
+				<li><a href="portofoliodetail.html">Automotriz</a></li>
+				<li><a href="portofoliodetail copy.html">Especializado</a></li>
+			</ul>
+			</li>
+			<li>
+			<a href="elements.html">Funcionalidades</a>
+			</li>
+			<li>
+			<a href="about.html">Sobre nosotros</a>
+			</li>
+
+			<li>
+			<a href="contact.html">Contacto</a>
+			</li>
+		</ul>
+		</nav>
+	</div>
+</div>
+<div class="clear">
+</div>
+<!-- SLIDER 
+================================================== -->
+<div id="ei-slider" class="ei-slider">
+	<ul class="ei-slider-large">
+		<li>
+		<img src="http://www.wowthemes.net/demo/studiofrancesca/images/slideshow/large/2.jpg" alt="image01" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>Dream</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image02" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>Hope</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image03" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>Smile</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image04" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>ask</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image05" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>refuse</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image06" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>give</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image07" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>receive</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image08" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to </h2>
+			<h3>Love</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image09" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Dare to</h2>
+			<h3>Forgive</h3>
+		</div>
+		</li>
+		<li>
+		<img src="http://placehold.it/1400x500/222" alt="image10" class="responsiveslide">
+		<div class="ei-title">
+			<h2>Stand up & </h2>
+			<h3>Live</h3>
+		</div>
+		</li>
+	</ul>
+	<!-- slider-thumbs -->
+	<ul class="ei-slider-thumbs">
+		<li class="ei-slider-element">Current</li>
+		<li><a href="#">Slide 1</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb01"/></li>
+		<li><a href="#">Slide 2</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb02"/></li>
+		<li><a href="#">Slide 3</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb03"/></li>
+		<li><a href="#">Slide 4</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb04"/></li>
+		<li><a href="#">Slide 5</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb05"/></li>
+		<li><a href="#">Slide 6</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb06"/></li>
+		<li><a href="#">Slide 7</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb07"/></li>
+		<li><a href="#">Slide 8</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb08"/></li>
+		<li><a href="#">Slide 9</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb09"/></li>
+		<li><a href="#">Slide 10</a><img src="http://placehold.it/150x59/444" class="slideshowthumb" alt="thumb10"/></li>
+	</ul>
+</div>
+<div class="minipause">
+</div>
+<!-- SUBHEADER
+================================================== -->
+<div id="subheader">
+	<div class="row">
+		<div class="twelve columns">
+			<p class="text-center">
+				 "La visión es el arte de ver lo que es invisible para los demás" - Jonathan Swift
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- FOOOTER 
+================================================== -->
+<div class="copyright">
+		
+	<div class="row">
+		<div >
+			 <span > Copyright 2022 DETODOENALAMBRE S.A. DE C.V.</span>
+		</div>
+	</div>
+</div>
+<!-- JAVASCRIPTS 
+================================================== -->
+<!-- Javascript files placed here for faster loading -->
+<script src="javascripts/foundation.min.js"></script>   
+<script src="javascripts/jquery.easing.1.3.js"></script>
+<script src="javascripts/elasticslideshow.js"></script>
+<script src="javascripts/jquery.carouFredSel-6.0.5-packed.js"></script>
+<script src="javascripts/jquery.cycle.js"></script>
+<script src="javascripts/app.js"></script>
+<script src="javascripts/modernizr.foundation.js"></script>
+<script src="javascripts/slidepanel.js"></script>
+<script src="javascripts/scrolltotop.js"></script>
+<script src="javascripts/hoverIntent.js"></script>
+<script src="javascripts/superfish.js"></script>
+<script src="javascripts/responsivemenu.js"></script>
+</body>
+</html>
